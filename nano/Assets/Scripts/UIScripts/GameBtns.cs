f9c7324bb777fbb2d2223717478d29c437b51984
danilocaverna <nq3i4fsx@hotmail.com>
2019-03-04 17:47:11 -0400

    pause button

--- nano/Assets/Scripts/UIScripts/GameBtns.cs
+++ nano/Assets/Scripts/UIScripts/GameBtns.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 public class GameBtns : MonoBehaviour{
     public event Action OnChangeAmmo = delegate{};
+    public event Action OnPause = delegate{};
     public void ChangeAmmo(){
         OnChangeAmmo();
+    }
+    public void Pause(){
+        Time.timeScale = (Time.timeScale + 1) % 2;
+        OnPause();
     }
 }
