9a742326a36ec2dc35e8bbce69e61c4a680cece8
danilocaverna <nq3i4fsx@hotmail.com>
2019-03-20 18:06:04 -0400

    aumento da fase 2 e virus explosao (não é animação)

---
+++ nano/Assets/Scripts/VirusGScripts/VirusExplode.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+public class VirusExplode : MonoBehaviour{
+    public IHaveLife HaveLife { get; set; }
+    private GameObject ProjectilePrefab;
+    public int numberOfProjectiles;
+    void Awake(){
+        HaveLife = GetComponent<IHaveLife>();
+        HaveLife.OnDied += Explode;
+
+        ProjectilePrefab = GetComponent<IShoot>() != null ? GetComponent<IShoot>().ProjectilePrefab : null;
+    }
+    public void Explode(){
+        if(ProjectilePrefab != null)
+        for (int i = 0; i < numberOfProjectiles; i++){
+            GameObject te = Instantiate(ProjectilePrefab);
+            te.AddComponent(typeof(VirusSummonEnterOnScreen));
+            te.transform.position = transform.position;    
+        }
+        
+    }
+}
