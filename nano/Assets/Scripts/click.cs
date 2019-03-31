f1408acda1e5d96f71a2285d08ae34a231f7aaea
danilocaverna <nq3i4fsx@hotmail.com>
2019-03-31 16:56:48 -0400

    fases de menu

---
+++ nano/Assets/Scripts/click.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click : MonoBehaviour{
+    public GameObject imagem;
+    void Update(){
+        if(Input.GetMouseButtonDown(0)){
+            float z = imagem.transform.position.z;
+            imagem.transform.position = new Vector3(imagem.transform.position.x, imagem.transform.position.y, (z+1f)%2f);
+        }
+    }
+}
